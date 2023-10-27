088d9411dba1f69297335f734874c339cb1168b4
Bulat Sitdikov <bulatsitdikov@MacBook-Air.local>
2023-10-27 20:34:05 +0500

    add scroll control

--- src/components/business/PageWrapper.jsx
+++ src/components/business/PageWrapper.jsx
@@ -1,15 +1,110 @@
 'use client';
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useRouter } from 'next/navigation';
+import { useScroll } from '@/src/hooks/useScroll';
+import { useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
 
 const PageWrapper = ({ children }) => {
   const router = useRouter();
-  console.log(router.pathname);
+  const path = usePathname();
+
+  useScroll();
+
+  //touch control
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  const minSwipeDistance = 150;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null);
+    setTouchStart(e.targetTouches[0].clientY);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientY);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isDownSwipe = distance > minSwipeDistance;
+    const isUpSwipe = distance < -minSwipeDistance;
+
+    if (path == '/') {
+      if (isDownSwipe) {
+        router.push('/project');
+      }
+    }
+
+    if (path == '/project') {
+      if (isUpSwipe) {
+        router.push('/');
+      }
+      if (isDownSwipe) {
+        router.push('/location');
+      }
+    }
+
+    if (path == '/location') {
+      if (isUpSwipe) {
+        router.push('/project');
+      }
+      if (isDownSwipe) {
+        router.push('/architecture');
+      }
+    }
+
+    if (path == '/architecture') {
+      if (isUpSwipe) {
+        router.push('/location');
+      }
+      if (isDownSwipe) {
+        router.push('/infrastructure');
+      }
+    }
+
+    if (path == '/infrastructure') {
+      if (isUpSwipe) {
+        router.push('/architecture');
+      }
+      if (isDownSwipe) {
+        router.push('/layouts');
+      }
+    }
+
+    if (path == '/layouts') {
+      if (isUpSwipe) {
+        router.push('/infrastructure');
+      }
+      if (isDownSwipe) {
+        router.push('/parking');
+      }
+    }
+
+    if (path == '/parking') {
+      if (isUpSwipe) {
+        router.push('/layouts');
+      }
+      if (isDownSwipe) {
+        router.push('/contacts');
+      }
+    }
+
+    if (path == '/contacts') {
+      if (isUpSwipe) {
+        router.push('/parking');
+      }
+    }
+  };
 
   return (
     <AnimatePresence wait={true}>
-      <motion.div key={router.pathname}>
+      <motion.div
+        key={router.pathname}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
         {/* <motion.div
           className='slide-in'
           initial={{ scaleY: 0 }}
---
+++ src/hooks/useScroll/index.jsx
@@ -0,0 +1,91 @@
+`use client`;
+
+import { useEffect } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+
+export const useScroll = () => {
+  const router = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll);
+    return () => window.removeEventListener('wheel', handleScroll);
+  });
+
+  const handleScroll = (event) => {
+    console.log('scroll event', event.deltaY);
+    console.log(path);
+
+    if (path == '/') {
+      if (event.deltaY > 10) {
+        router.push('/project');
+      }
+    }
+
+    if (path == '/project') {
+      if (event.deltaY < -10) {
+        router.push('/');
+      }
+
+      if (event.deltaY > 10) {
+        router.push('/location');
+      }
+    }
+
+    if (path == '/location') {
+      if (event.deltaY < -10) {
+        router.push('/project');
+      }
+
+      if (event.deltaY > 10) {
+        router.push('/infrastructure');
+      }
+    }
+
+    if (path == '/infrastructure') {
+      if (event.deltaY < -10) {
+        router.push('/location');
+      }
+
+      if (event.deltaY > 10) {
+        router.push('/architecture');
+      }
+    }
+
+    if (path == '/architecture') {
+      if (event.deltaY < -10) {
+        router.push('/infrastructure');
+      }
+
+      if (event.deltaY > 10) {
+        router.push('/layouts');
+      }
+    }
+
+    if (path == '/layouts') {
+      if (event.deltaY < -10) {
+        router.push('/architecture');
+      }
+
+      if (event.deltaY > 60) {
+        router.push('/parking');
+      }
+    }
+
+    if (path == '/parking') {
+      if (event.deltaY < -10) {
+        router.push('/layouts');
+      }
+
+      if (event.deltaY > 10) {
+        router.push('/contacts');
+      }
+    }
+
+    if (path == '/contacts') {
+      if (event.deltaY < -80) {
+        router.push('/layouts');
+      }
+    }
+  };
+};
