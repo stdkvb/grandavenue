d0190814bfc0f2e866439fd7b72a1db49ce283f3
Bulat Sitdikov <bulatsitdikov@MacBook-Air.local>
2023-10-27 13:09:38 +0500

    bugs fix

--- src/components/business/PageWrapper.jsx
+++ src/components/business/PageWrapper.jsx
@@ -1,17 +1,15 @@
 'use client';
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-// import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const PageWrapper = ({ children }) => {
-  // const router = useRouter();
-  const path = usePathname();
-  console.log(path);
+  const router = useRouter();
+  console.log(router.pathname);
 
   return (
     <AnimatePresence wait={true}>
-      <motion.div key={path}>
+      <motion.div key={router.pathname}>
         <motion.div
           className='slide-in'
           initial={{ scaleY: 0 }}
