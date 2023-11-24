a399a3ef50bf4fc4354feda6ea759476d014489d
Bulat Sitdikov <bulatsitdikov@MacBook-Air.local>
2023-11-24 08:59:03 +0500

    update stat script

--- src/components/RoiStatScript.jsx
+++ src/components/RoiStatScript.jsx
@@ -1,33 +1,17 @@
 'use client';
+import Script from 'next/script';
 
 const RoiStatScript = () => {
-  if (typeof window !== 'undefined') {
-    const RoiStatScript = (function (w, d, s, h, id) {
-      w.roistatProjectId = id;
-      w.roistatHost = h;
-      var p = d.location.protocol == 'https:' ? 'https://' : 'http://';
-      var u = /^.roistat_visit=[^;]+(.)?$/.test(d.cookie)
-        ? '/dist/module.js'
-        : '/api/site/1.0/' +
-          id +
-          '/init?referrer=' +
-          encodeURIComponent(d.location.href);
-      var js = d.createElement(s);
-      js.charset = 'UTF-8';
-      js.async = 1;
-      js.src = p + h + u;
-      var js2 = d.getElementsByTagName(s)[0];
-      js2.parentNode.insertBefore(js, js2);
-    })(
-      window,
-      document,
-      'script',
-      'cloud.roistat.com',
-      'b2521525d1cf497c95dbac0a2e38b280'
-    );
-  }
-
-  return;
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        type='text/javascript'
+        src='scripts/roistatscript.js'
+        async
+      />
+    </>
+  );
 };
 
 export default RoiStatScript;
